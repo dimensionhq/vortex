4c1f76c7e4f16a189a16c4fe642d732387ed765f
Varun Potti <potti.varun07@gmail.com>
2021-12-19 21:39:39 +0530

    feat: Got packages working

--- vortex/vite.config.ts
+++ vortex/vite.config.ts
@@ -1,38 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 import dts from "vite-plugin-dts";
-
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react(), dts()],
   build: {
     lib: {
-      entry: "./src/Blog/Blog.tsx",
-      formats: ["es", "cjs"],
+      entry: "./src/index.ts",
+      formats: ["cjs", "es"],
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ["react"],
+      external: ["react", "react-dom"],
     },
   },
-  // build: {
-  //   lib: {
-  //     entry: path.resolve(__dirname, "src/Blog/Blog.tsx"),
-  //     name: "MyLib",
-  //     fileName: (format) => `my-lib.${format}.js`,
-  //   },
-  //   rollupOptions: {
-  //     external: ["React"],
-  //     output: {
-  //       // Provide global variables to use in the UMD build
-  //       // Add external deps here
-  //       globals: {
-  //         react: "React",
-  //       },
-  //     },
-  //   },
-  // },
-  plugins: [react(), dts()],
   define: {
     "process.env": {},
   },
